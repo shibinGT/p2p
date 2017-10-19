07a693190ff920f6d97be15d459fa4e9a8ec6cef
xwp <xwp1327@163.com>
2017-10-19 18:30:27 +0800

    修改了mapper

--- p2p-common/src/main/java/com/qhcs/ssm/dao/GroupMapper.java
+++ p2p-common/src/main/java/com/qhcs/ssm/dao/GroupMapper.java
@@ -10,38 +10,39 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.qhcs.ssm.entity.Group;
-import com.qhcs.ssm.entity.Role;
-import com.qhcs.ssm.entity.User;
 
 /**
  * 分组的dao层的mapper接口
+ * 
  * @version 2017年10月8日下午7:06:36
  * @author xuweiping
  */
 public interface GroupMapper {
-	
+
 	/**
 	 * 
 	 * 根据分组编码模糊查询分组列表
+	 * 
 	 * @version 2017年10月8日下午7:07:42
 	 * @author xuweiping
-	 * @param groupCode 分组编码
+	 * @param groupCode
+	 *            分组编码
 	 * @return 分组列表
 	 */
 	public List<Group> quaryList(@Param("groupCode") String groupCode);
-	
-	
+
 	/**
 	 * 
 	 * 根据分组id查询分组信息
+	 * 
 	 * @version 2017年10月8日下午10:47:41
 	 * @author xuweiping
-	 * @param groupId 分组id
+	 * @param groupId
+	 *            分组id
 	 * @return group 分组信息
 	 */
 	public Group quaryById(Integer groupId);
-	
-	
+
 	/**
 	 * 
 	 * 根据ID删除
@@ -62,66 +63,75 @@ public interface GroupMapper {
 	 * @return int 返回删除成功的数量
 	 */
 	public int batchDelete(@Param("lists") Integer[] lists);
-	
+
 	/**
 	 * 
 	 * 增加一个分组信息
+	 * 
 	 * @version 2017年10月8日下午7:11:33
 	 * @author xuweiping
-	 * @param group 分组信息
+	 * @param group
+	 *            分组信息
 	 * @return 是否添加成功
 	 */
 	public boolean addGroup(Group group);
-	
+
 	/**
 	 * 
 	 * 批量增加一个分组的角色信息
+	 * 
 	 * @version 2017年10月8日下午8:15:47
 	 * @author xuweiping
-	 * @param group 分组信息
+	 * @param group
+	 *            分组信息
 	 * @return 是否添加成功
 	 */
-	public boolean batchAddRoles2(@Param("roleIds") List<Integer> roleIds,@Param("id") Integer id);
-	
+	public boolean batchAddRoles2(@Param("roleIds") List<Integer> roleIds, @Param("id") Integer id);
+
 	/**
 	 * 
 	 * 根据分组id删除分组里面的所有角色
+	 * 
 	 * @version 2017年10月8日下午8:41:26
 	 * @author xuweiping
-	 * @param groupId 分组id
+	 * @param groupId
+	 *            分组id
 	 * @return 是否删除成功
 	 */
 	public boolean delRoles(Integer groupId);
-	
+
 	/**
 	 * 
 	 * 批量增加一个分组的用户信息
+	 * 
 	 * @version 2017年10月8日下午8:30:45
 	 * @author xuweiping
-	 * @param group 分组信息
+	 * @param group
+	 *            分组信息
 	 * @return 是否添加成功
 	 */
-	public boolean batchAddUsers(@Param("userIds") List<Integer> userIds,@Param("id") Integer id);
-	
+	public boolean batchAddUsers(@Param("userIds") List<Integer> userIds, @Param("id") Integer id);
+
 	/**
 	 * 
 	 * 根据分组id删除分组里面的所有用户
+	 * 
 	 * @version 2017年10月8日下午8:41:26
 	 * @author xuweiping
-	 * @param groupId 分组id
+	 * @param groupId
+	 *            分组id
 	 * @return 是否删除成功
 	 */
 	public boolean delUsers(Integer groupId);
-	
-	
-	
-	
+
 	/**
 	 * 
 	 * 修改一个分组信息
+	 * 
 	 * @version 2017年10月8日下午7:12:45
 	 * @author xuweiping
-	 * @param group 修改后的分组信息
+	 * @param group
+	 *            修改后的分组信息
 	 * @return 是否修改成功
 	 */
 	public boolean updateGroup(Group group);
